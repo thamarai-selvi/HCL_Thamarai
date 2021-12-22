a5f8eff341b6bec848745f61c34185f0e59f399e
thamarai-selvi <92290186+thamarai-selvi@users.noreply.github.com>
2021-12-22 11:22:49 +0530

    Add files via upload

---
+++ helloclass.java
@@ -0,0 +1,15 @@
+package helloworld;
+import java.util.Scanner;
+ 
+public class helloclass {
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		
+
+
+	}
+
+}
